3df7fccba89a36e06fc65ecd67a8580a9969965a
Darren Coxall <darren@darrencoxall.com>
2015-02-08 16:43:44 +0000

    Initial import of site

---
+++ js/coxall.js
@@ -0,0 +1,31 @@
+WebFontConfig = {
+  google: {
+    families: [
+      'Source+Sans+Pro:400,700:latin',
+      'Droid+Serif:400,700,400italic,700italic:latin'
+    ]
+  }
+};
+
+// Google Web Fonts
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
+
+// Highlight JS
+hljs.initHighlightingOnLoad();
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-25937132-1', 'auto');
+ga('send', 'pageview');
